ab067270987d9be9f63955b4a597017a23da65c9
Maximo Rodriguez <maximo1225@gmail.com>
2021-10-28 16:25:43 -0400

    Cleaning Components

--- force-app/main/default/lwc/reminderDash/reminderDash.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement, api } from 'lwc';
-
-export default class ReminderDash extends LightningElement {
-    @api todayAmount = 0;
-    @api upcommingAmount = 0;
-    @api scheduledAmount = 0;
-    @api allAmount = 0;
-}
